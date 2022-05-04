a71cb062c3700e245275ae155a5a6d14717d21c1
Lukas22D <luk.look.s.rocha@gmail.com>
2022-05-04 15:34:39 -0300

    Nova atividade

---
+++ colecoes/sets.js
@@ -0,0 +1,8 @@
+const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+function valoresUnicos(array){
+    const set = new Set(array);
+    return [...set];
+}
+
+console.log(valoresUnicos(meuArray));
